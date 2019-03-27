efaa02af2a6618ee848c43be5c4c84e981993792
Dan Flynn <danfbflynn@gmail.com>
2019-03-26 20:54:37 -0400

    Skipping the offset step

--- Bike_counter_get.R
+++ Bike_counter_get.R
@@ -14,22 +14,30 @@ setwd("~/Projects/BikeCount")
 
 # Grab data from API ----
 
-# Load historical count once. Takes 7s initially to download, vs. 0.1s to load from working directory. # We will increment the historical data daily
-if(length(grep("BikeCountHist.RData", dir()))==0){
-  hist_count <- read.socrata("https://data.cambridgema.gov/resource/gxzm-dpwp.csv")
-  save("hist_count", file = "BikeCountHist.RData")
-} else { load("BikeCountHist.RData") }
+FRESHENUP = F
 
-rows_hist <- nrow(hist_count)
-last_day <- max(hist_count$date)
+if(FRESHENUP){
+  # Load historical count once. Takes 7s initially to download, vs. 0.1s to load from working directory. 
+  # We will increment the historical data daily
+  if(length(grep("BikeCountHist.RData", dir()))==0){
+    hist_count <- read.socrata("https://data.cambridgema.gov/resource/gxzm-dpwp.csv")
+    save("hist_count", file = "BikeCountHist.RData")
+  } else { load("BikeCountHist.RData") }
+  
+  rows_hist <- nrow(hist_count)
+  last_day <- max(hist_count$date)
+  
+  # Try to get fresher data. Defaults to hist_count if nothing fresher found.
+  if(Sys.Date() > as.Date(last_day)){
+  #  new_count <- read.socrata(url = paste0("https://data.cambridgema.gov/resource/gxzm-dpwp.csv?$offset=", rows_hist))
+    if(nrow(new_count) > 0){
+      count <- rbind(hist_count, new_count)
+      } else { count <- hist_count }
+    }
+} else {
+  count <- read.socrata(url = "https://data.cambridgema.gov/resource/gxzm-dpwp.csv")
+}
 
-# Try to get fresher data. Defaults to hist_count if nothing fresher found.
-if(Sys.Date() > as.Date(last_day)){
-  new_count <- read.socrata(paste0("https://data.cambridgema.gov/resource/gxzm-dpwp.csv?$offset=", rows_hist))
-  if(nrow(new_count) > 0){
-    count <- rbind(hist_count, new_count)
-    } else { count <- hist_count }
-  }
 
 # Get some metrics ----
 
@@ -40,12 +48,15 @@ if(Sys.Date() > as.Date(last_day)){
 # Year to date total trips and last year's comparison value: latest_ytd and ytd_compare
 
 # read.socrata reads 'date' as date-time, all at midnight. Need to reformat as actualy date only, without time.
-count$date <- as.Date(count$date)
+count <- as_tibble(count)
 
-daily <- count %>% 
-  mutate(year = year(datetime)) %>%
+count <- count %>% 
+  mutate(date = as.Date(date),
+         year = year(datetime))
+
+daily = count %>%
   group_by(year, date) %>%
-  summarize(daily_total = sum(total))
+  dplyr::summarise(daily_total = sum(total))
 
 max_day = daily %>% filter(daily_total == max(daily_total))
 
@@ -56,14 +67,14 @@ last_year_compare = daily %>% filter(date == paste(year(latest_day$date)-1, mont
 latest_ytd = daily %>% 
   ungroup(daily) %>% 
   filter(year == max(year)) %>%
-  summarize(nrecord = n(), 
+  dplyr::summarize(nrecord = n(), 
             ytd = sum(daily_total))
 
 ytd_compare = daily %>% 
   ungroup(daily) %>%
   filter(year == max(year)-1) %>%
   filter(date <= paste(year(latest_day$date)-1, month(latest_day$date), day(latest_day$date), sep="-")) %>%
-  summarize(nrecord = n(),
+  dplyr::summarize(nrecord = n(),
             ytd = sum(daily_total))
 
 # Weekly counts and comparison
@@ -72,7 +83,7 @@ weekly <- count %>%
   mutate(weekofyear = week(datetime),
          year = year(datetime)) %>%
   group_by(year, weekofyear) %>%
-  summarize(weekly_total = sum(total),
+  dplyr::summarize(weekly_total = sum(total),
             complete_week = length(total) == 24*7*4)
 
 max_week = weekly %>% filter(weekly_total == max(weekly_total)) # gives the max week of each year, since we did group_by year
---
+++ tests/test_Bike_counter.R
@@ -0,0 +1,7 @@
+# Testing the Bike Counter code
+# Building off of these resources
+# http://r-pkgs.had.co.nz/tests.html
+# https://dzone.com/articles/example-unit-testing-r-code
+# https://journal.r-project.org/archive/2011-1/RJournal_2011-1_Wickham.pdf
+
+source('Bike_counter_get.R')
