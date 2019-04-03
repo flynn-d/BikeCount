730b63081b9252aa63184711dced6b2aa652e0c8
Dan Flynn <danfbflynn@gmail.com>
2019-04-02 22:23:10 -0400

    plotly test

--- app.R
+++ app.R
@@ -10,22 +10,25 @@ ui <- pageWithSidebar(
                 selected = 'total')
   ),
   mainPanel(
-    plotOutput('plot1')
+    plotlyOutput('plot1')
   )
 )
 
 server <- function(input, output, session) {
     source('Bike_counter_get.R')
 
-    selectedData <- reactive({
-      daily[, c(input$xcol, input$ycol)]
-    })
-
-    output$plot1 <- renderPlot({
-      # gp = ggplot(selectedData(), aes(x = selectedData()[,1], y = selectedData()[,2])) +
-      #   geom_point()
-      # ggplotly(gp)
-      plot(selectedData(), aes(x = selectedData()[,1], y = selectedData()[,2]))
+    # selectedData <- reactive({
+    #   daily[, c(input$xcol, input$ycol)]
+    # })
+    #usevars <- reactive({c(input$xcol, input$ycol)})
+      
+    output$plot1 <- renderPlotly({
+      gp = ggplot(daily, 
+                  aes_string(x = as.name(input$xcol), y = as.name(input$ycol))) +
+        geom_point() + theme_bw() 
+      
+      print(ggplotly(gp))
+      #plot(selectedData(), aes(x = selectedData()[,1], y = selectedData()[,2]))
           })
 
 }
